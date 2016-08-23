ecbd9f259aaff29111ed7ddec4736efc582c4bba
davidemoraschi <davidem@eurostrategy.net>
2016-08-23 15:08:26 +0200

    moved to GitHub

---
+++ oData_BCS/Features/Feature1/Feature1.feature
@@ -0,0 +1,9 @@
+﻿<?xml version="1.0" encoding="utf-8"?>
+<feature xmlns:dm0="http://schemas.microsoft.com/VisualStudio/2008/DslTools/Core" dslVersion="1.0.0.0" Id="d437a69a-8e60-4120-acdd-69c4e25a78a6" description="Característica de la aplicación de SharePoint" featureId="d437a69a-8e60-4120-acdd-69c4e25a78a6" imageUrl="" solutionId="00000000-0000-0000-0000-000000000000" title="oData_BCS Feature1" version="" deploymentPath="$SharePoint.Project.FileNameWithoutExtension$_$SharePoint.Feature.FileNameWithoutExtension$" xmlns="http://schemas.microsoft.com/VisualStudio/2008/SharePointTools/FeatureModel">
+  <projectItems>
+    <projectItemReference itemId="7aae81df-7b30-43fa-ab52-034501f4db3a" />
+    <projectItemReference itemId="26bfecee-999d-491b-ac41-457560a365a2" />
+    <projectItemReference itemId="2220087c-8a3f-4afc-baaf-8123eee66644" />
+    <projectItemReference itemId="3a3c630c-cfac-4d2e-96d6-da5aa517592c" />
+  </projectItems>
+</feature>
---
+++ oData_BCS/Scripts/App.js
@@ -0,0 +1,29 @@
+﻿var context;
+var web;
+var user;
+
+// Este código se ejecuta cuando el DOM está preparado y crea un objeto de contexto necesario para poder usar el modelo de objetos de SharePoint
+$(document).ready(function () {
+    context = SP.ClientContext.get_current();
+    web = context.get_web();
+
+    getUserName();
+});
+
+// Esta función prepara, carga y ejecuta una consulta de SharePoint para obtener información del usuario actual
+function getUserName() {
+    user = web.get_currentUser();
+    context.load(user);
+    context.executeQueryAsync(onGetUserNameSuccess, onGetUserNameFail);
+}
+
+// Esta función se ejecuta si la llamada OM anterior se realiza correctamente
+// Reemplaza el contenido del elemento 'helloString' con el nombre de usuario
+function onGetUserNameSuccess() {
+    $('#message').text('Hello ' + user.get_title());
+}
+
+// Esta función se ejecuta si se produce un error en la llamada anterior
+function onGetUserNameFail(sender, args) {
+    alert('Failed to get user name. Error:' + args.get_message());
+}
